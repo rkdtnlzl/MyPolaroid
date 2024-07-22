38d3c5117fe2decde943b7c20b1ca88b672ee504
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-22 17:40:45 +0900

    [Feat/#1] 닉네임 유효성검사 Extension 추가

---
+++ MyPolaroid/Extension/String+.swift
@@ -0,0 +1,25 @@
+//
+//  String+.swift
+//  MyPolaroid
+//
+//  Created by 강석호 on 7/22/24.
+//
+
+import Foundation
+
+extension String {
+    func validateNickname() -> NicknameValidationResult {
+        let specialLiterals = CharacterSet(charactersIn: "@#$%")
+        let numbers = CharacterSet.decimalDigits
+        
+        if self.count < 2 || self.count > 9 {
+            return .invalidLength
+        } else if self.rangeOfCharacter(from: specialLiterals) != nil {
+            return .containsSpecialCharacters
+        } else if self.rangeOfCharacter(from: numbers) != nil {
+            return .containsNumbers
+        } else {
+            return .valid
+        }
+    }
+}
