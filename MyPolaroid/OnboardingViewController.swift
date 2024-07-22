2d3085355b4bd016ef3f4f360942438aa15f8983
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-22 15:32:19 +0900

    [Feat] OnboardingViewController 생성

---
+++ MyPolaroid/OnboardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController.swift
+//  MyPolaroid
+//
+//  Created by 강석호 on 7/22/24.
+//
+
+import UIKit
+
+final class OnboardingViewController: BaseViewController {
+    
+    override func configureNavigation() {
+        navigationItem.title = "asdf"
+    }
+}
