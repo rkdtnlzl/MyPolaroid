0d14ce290a290ca11071e2ebf6c133af0cb81b73
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-22 17:41:50 +0900

    [Feat/#1] MyPolaroid Custom Colors 추가

---
+++ MyPolaroid/Global/MPColors.swift
@@ -0,0 +1,17 @@
+//
+//  MPColors.swift
+//  MyPolaroid
+//
+//  Created by 강석호 on 7/22/24.
+//
+
+import UIKit
+
+enum MPColors {
+    
+    static let blue = UIColor(hexCode: "186FF2")
+    static let gray = UIColor(hexCode: "8C8C8C")
+    static let darkGray = UIColor(hexCode: "4D5652")
+    static let lightGray = UIColor(hexCode: "F2F2F2")
+    static let red = UIColor(hexCode: "F04452")
+}
